6fc330c3ff12cea3774b6c87b76d40eeaecd3bc9
seok5074 <52529324+seok5074@users.noreply.github.com>
2019-07-16 12:56:16 +0900

    html(start2)
    190624

---
+++ 20190624_HtmlTest/WebContent/js/common_regular.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+var dan_regular=/^[2-9]{1}$/;
+
+var email_regular = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+var	cell_regular = /^\d{3}-\d{3,4}-\d{4}$/;
+
+var tell_regular = /^\d{2,3}-\d{3,4}-\d{4}$/;
+
+
